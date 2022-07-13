794b535dff90440c7d484c1a42c247c9cf647c8d
Ashu <abhishekaggarwal2801@gmail.com>
2022-07-13 18:12:22 +0530

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./common/Nav";
+import Home from "./Home";
+import "./common/Commoncss.css";
+import About from "./About";
+import Services from "./Services";
+import Work from "./Work";
+import Education from "./Education";
+import Jsprojects from "./Jsprojects";
+import Contact from "./Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+/*import Services from "./Pages/Services";
+import Education from "./Pages/Education";
+import Work from "./Pages/Work";
+import Contact from "./Pages/Contact";
+import Projects from "./Pages/Projects";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import React, { Route, Routes } from "react-router-dom";
+import PageNotFound from "./Pages/PageNotFound";
+import Jsprojects from "./Pages/Jsprojects";*/
+
+export default function App(){
+  return(
+    <div>
+      <Nav/>
+      <Home/>
+      <About/>
+      <Services/>
+      <Work/>
+      <Education/>
+      <Jsprojects/>
+      <Contact/>
     </div>
-  );
+  )
 }
-
-export default App;
