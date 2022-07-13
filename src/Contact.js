a7c7afacf4fa5c99c775ed4035b8f008cd936832
Ashu <abhishekaggarwal2801@gmail.com>
2022-07-14 00:37:45 +0530

    made Send-Email functional

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,22 @@
 import React, { useRef, useState } from "react";
 import "./Contact.css";
+import emailjs from "emailjs-com"
 
 const Contact = () => {
   const [done, setDone] = useState(false);
-  const formRef = useRef();
+  const form = useRef();
 
   const sendEmail = (e) => {
-    e.target.reset();
+    e.preventDefault();
+
+    emailjs.sendForm('Koro-Coder', 'template_h381fae', form.current, '7u6biypENMyke-Flw')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      setDone(true);
+      e.target.reset();
   };
 
   return (
@@ -16,32 +26,23 @@ const Contact = () => {
           <div className="col-sm-6 offset-sm-3">
             <h2 data-aos="fade-up">Contact</h2>
             <br />
-            <form onSubmit={sendEmail} ref={formRef}>
+            <form onSubmit={sendEmail} ref={form}>
               {/* <form ref={formRef}> */}
               <input
                 data-aos="fade-up"
                 type="text"
                 placeholder="Your Name"
                 className="form-control"
-                name="user_name" required
+                name="name" required
               />
               <div className="row">
-                <div className="col-sm-6">
+                <div >
                   <input
                     data-aos="fade-up"
                     type="email"
                     placeholder="Your Email"
                     className="form-control"
-                    name="user_email" required
-                  />
-                </div>
-                <div className="col-sm-6">
-                  <input
-                    data-aos="fade-up"
-                    type="text"
-                    placeholder="Contact Contact"
-                    className="form-control"
-                    name="user_contact" pattern="[0-9]{10}" title="Please enter valid number." required
+                    name="email" required
                   />
                 </div>
               </div>
@@ -50,11 +51,11 @@ const Contact = () => {
                 type="text"
                 placeholder="Subject"
                 className="form-control"
-                name="user_subject" required
+                name="subject" required
               />
               <textarea
                 data-aos="fade-up"
-                name="user_message"
+                name="message"
                 rows="4"
                 className="form-control"
                 placeholder="Your Message" required
